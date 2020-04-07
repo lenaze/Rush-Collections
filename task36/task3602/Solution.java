be149a1ace6c2146946acf87fc6850eb5d3c2d30
lenaz <lena.zelenaya@gmail.com>
2020-04-07 12:54:40 +0300

    Took all the classes from Collections

--- task36/task3602/Solution.java
+++ task36/task3602/Solution.java
@@ -1,6 +1,8 @@
 package com.javarush.task.task36.task3602;
 
-/* 
+import java.util.Collections;
+
+/*
 Найти класс по описанию
 */
 public class Solution {
@@ -9,6 +11,15 @@ public class Solution {
     }
 
     public static Class getExpectedClass() {
+        Class<?>[] classes = Collections.class.getDeclaredClasses();
+
+        for (Class<?> c : classes) {
+            try{
+
+            }catch (NullPointerException e) {
+                System.err.println(e.getMessage());
+            }
+        }
         return null;
     }
 }
