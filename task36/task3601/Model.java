5b9996d1e1a14ff9ed933a4312714b36928905f5
lenaz <lena.zelenaya@gmail.com>
2020-04-07 12:11:11 +0300

    Simple presentation of MVC with methods from Solution ready! We can see how it works.

--- task36/task3601/Model.java
+++ task36/task3601/Model.java
@@ -1,4 +1,11 @@
 package com.javarush.task.task36.task3601;
 
+import java.util.List;
+
 public class Model {
+    public List<String> getStringDataList() {
+        Service service = new Service();
+
+        return service.getData();
+    }
 }
--- task36/task3601/Solution.java
+++ task36/task3601/Solution.java
@@ -8,27 +8,6 @@ MVC - простая версия
 */
 public class Solution {
     public static void main(String[] args) {
-        new Solution().fireShowDataEvent();
-    }
-
-    public List<String> getData() {
-        List<String> data = new ArrayList<String>() {{
-            add("First string");
-            add("Second string");
-            add("Third string");
-        }};
-        return data;
-    }
-
-    public void fireShowDataEvent() {
-        System.out.println(onShowDataList());
-    }
-
-    public List<String> getStringDataList() {
-        return getData();
-    }
-
-    public List<String> onShowDataList() {
-        return getStringDataList();
+        new View().fireShowDataEvent();
     }
 }
