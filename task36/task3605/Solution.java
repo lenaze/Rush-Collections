af8c516c4ac45a7859ef2168666a47f5b599cf2c
lenaz <lena.zelenaya@gmail.com>
2020-04-09 13:16:59 +0300

    With some modifications and with help of Stream API, got the result

--- task36/task3605/Solution.java
+++ task36/task3605/Solution.java
@@ -2,8 +2,11 @@ package com.javarush.task.task36.task3605;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /* 
 Использование TreeSet
@@ -11,16 +14,17 @@ import java.util.TreeSet;
 public class Solution {
     public static void main(String[] args) throws IOException {
         File file = new File(args[0]);
-        InputStreamReader reader = new InputStreamReader(new FileInputStream(file));
-
         TreeSet<Character> set = new TreeSet<>();
-        while(reader.ready()){
-            set.add((char) reader.read());
+
+        try(InputStreamReader reader = new InputStreamReader(new FileInputStream(file))) {
+            int c;
+            while (reader.ready()) {
+                c = reader.read();
+                if ((c > 64 && c < 91) || (c > 96 && c < 123)) {
+                    set.add(Character.toLowerCase((char) c));
+                }
+            }
         }
-        reader.close();
-
-
-
-
+        set.stream().limit(5).forEach(System.out::print);
     }
 }
