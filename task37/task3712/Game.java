ac6ef5fc41332e70725aa677bde381d2b815bf97
lenaz <lena.zelenaya@gmail.com>
2020-04-10 14:00:01 +0300

    Realization of Template pattern

---
+++ task37/task3712/Game.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task37.task3712;
+
+public abstract class Game {
+    abstract public void prepareForTheGame();
+    abstract public void playGame();
+    abstract public void congratulateWinner();
+
+    public void run(){
+        this.prepareForTheGame();
+        this.playGame();
+        this.congratulateWinner();
+    }
+}
